213e1d6fc72eeee6e340a47164f511505355bb47
Sanjay Joshi <joshisa@us.ibm.com>
2015-12-23 10:26:41 -0500

    Updated script to utilize tar over ssh for file transfer

--- bluezone/configtweaks/.profile.d/fusemount.sh
+++ bluezone/configtweaks/.profile.d/fusemount.sh
@@ -39,14 +39,22 @@ if [ -n "${SSHFS_PRIV+set}" ]; then
   mkdir -p /home/vcap/misc
   echo -e "${delivery}${Yellow}  Initiating SSHFS mount ..."
   # Reference: http://manpages.ubuntu.com/manpages/karmic/en/man1/sshfs.1.html
-  sshfs ${SSHFS_USER}@${SSHFS_HOST}:${SSHFS_DIR} /home/vcap/misc -o IdentityFile=/home/vcap/app/.profile.d/id_rsa,StrictHostKeyChecking=yes,UserKnownHostsFile=/home/vcap/app/.profile.d/known_hosts,idmap=user,compression=no
+  # SSHFS Tuning Reference: http://www.admin-magazine.com/HPC/Articles/Sharing-Data-with-SSHFS
+  sshfs ${SSHFS_USER}@${SSHFS_HOST}:${SSHFS_DIR} /home/vcap/misc -o IdentityFile=/home/vcap/app/.profile.d/id_rsa,StrictHostKeyChecking=yes,UserKnownHostsFile=/home/vcap/app/.profile.d/known_hosts,idmap=user,compression=yes,cache=yes,kernel_cache,large_read,Ciphers=arcfour,cache_timeout=115200,attr_timeout=115200
   echo -e "${delivery}${Yellow}  Creating Domain Namespace within mounted location ..."
   mkdir -p /home/vcap/misc/${DRUPAL_DOMAIN_NAME}
   mkdir -p /home/vcap/misc/${DRUPAL_DOMAIN_NAME}/sites
   echo -e "${delivery}${Yellow}  Creating Symlink between Drupal Sites folder and mounted location ..."
   ln -s /home/vcap/misc/${DRUPAL_DOMAIN_NAME}/sites /home/vcap/app/htdocs/drupal-7.41
-  echo -e "${delivery}${Yellow}  Moving previously assembled sites folder content onto SSHFS mount (Overwrite enabled)..."
-  yes | cp -R /home/vcap/app/htdocs/drupal-7.41/mirage/. /home/vcap/app/htdocs/drupal-7.41/sites
+  echo -e "${delivery}${Yellow}  Moving previously assembled sites folder content onto SSHFS mount (Overwrite enabled).  Estimated time: 135 seconds ..."
+  # Comment:  cp is too slow, even with 180 sec extended health check
+  # cp -R /home/vcap/app/htdocs/drupal-7.41/mirage/. /home/vcap/app/htdocs/drupal-7.41/sites
+  # Comment:  scp was too slow, even with 180 sec extended health check
+  # scp -r /home/vcap/app/htdocs/drupal-7.41/mirage/. /home/vcap/app/htdocs/drupal-7.41/sites
+  # Comment:  Tar over ssh fits within the 180 second timeout health check.  ~135 seconds to move all default modules
+  # If health check timeouts occur during deploy, one mitigation is to reduce the amount of "desired" modules within the default assembly.
+  # In theory, now that we are using a SSHFS file storage, installation via website should persist.  This should also enable multiple cf instances
+  tar -C /home/vcap/app/htdocs/drupal-7.41/mirage -jcf - ./ | ssh -i /home/vcap/app/.profile.d/id_rsa -o UserKnownHostsFile=/home/vcap/app/.profile.d/known_hosts ${SSHFS_USER}@${SSHFS_HOST} "tar -C/home/paramount/${DRUPAL_DOMAIN_NAME}/sites -ojxf -"
   rm -rf /home/vcap/app/htdocs/drupal-7.41/mirage
   echo -e "${delivery}${Yellow}  Per best practices - Create Domain specific sites folder"
   mkdir /home/vcap/app/htdocs/drupal-7.41/sites/${DRUPAL_DOMAIN_NAME}
@@ -55,7 +63,7 @@ if [ -n "${SSHFS_PRIV+set}" ]; then
   mkdir /home/vcap/app/htdocs/drupal-7.41/sites/${DRUPAL_DOMAIN_NAME}/files
   mkdir /home/vcap/app/htdocs/drupal-7.41/sites/${DRUPAL_DOMAIN_NAME}/modules
   mkdir /home/vcap/app/htdocs/drupal-7.41/sites/${DRUPAL_DOMAIN_NAME}/libraries
-  touch /home/vcap/app/htdocs/drupal-7.41/sites/${DRUPAL_DOMAIN_NAME}/settings.php
+  touch /home/vcap/app/htdocs/drupal-7.41/sites/${DRUPAL_DOMAIN_NAME}/placeholder.settings.php
 else
   echo -e "${delivery}${Yellow}  No SSHFS Environment Variables detected. Proceeding with local ephemeral sites folder."
 fi
@@ -65,4 +73,3 @@ fi
 # ssh-keygen -b 2048 -t rsa -f /home/vcap/app/.profile.d/sshkey -q -N ""
 # Command below uses debug options which causes process to run in foreground ... good for debugging but not useful for running with cf apps.
 # sshfs root@134.168.17.44:/home/paramount /home/vcap/misc -o IdentityFile=/home/vcap/app/.profile.d/id_rsa,StrictHostKeyChecking=yes,UserKnownHostsFile=/home/vcap/app/.profile.d/known_hosts,idmap=user,compression=no,sshfs_debug,debug
-
